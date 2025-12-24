2f83f8463902f1527a328266d7adefef48dd83b3
Andriy <kykyagoloviy@gmail.com>
2025-12-24 17:59:36 +0200

    Fix

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,15 +1,28 @@
 import { Router } from "express";
-import { authenticate } from "../middlewares/authenticate.js";
-import { getAllNotes, getNoteById, createNote, updateNote, deleteNote } from "../controllers/notesController.js";
+import { celebrate } from "celebrate";
+import { authenticate } from "../middleware/authenticate.js";
+import {
+  getAllNotes,
+  getNoteById,
+  createNote,
+  updateNote,
+  deleteNote,
+} from "../controllers/notesController.js";
+import {
+  getAllNotesSchema,
+  noteIdSchema,
+  createNoteSchema,
+  updateNoteSchema,
+} from "../validations/notesValidation.js";
 
 const router = Router();
 
 router.use(authenticate);
 
-router.get("/notes", getAllNotes);
-router.get("/notes/:noteId", getNoteById);
-router.post("/notes", createNote);
-router.patch("/notes/:noteId", updateNote);
-router.delete("/notes/:noteId", deleteNote);
+router.get("/notes", celebrate(getAllNotesSchema), getAllNotes);
+router.get("/notes/:noteId", celebrate(noteIdSchema), getNoteById);
+router.post("/notes", celebrate(createNoteSchema), createNote);
+router.patch("/notes/:noteId", celebrate(updateNoteSchema), updateNote);
+router.delete("/notes/:noteId", celebrate(noteIdSchema), deleteNote);
 
 export default router;
