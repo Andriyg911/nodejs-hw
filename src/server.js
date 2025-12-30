e78aa7ea7472f79c3a9cc305e3c97d6fca0b657d
Andriy <kykyagoloviy@gmail.com>
2025-12-30 14:43:32 +0200

    fix: align logger import with default export

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import { errors } from "celebrate";
 
 import { connectMongoDB } from "./db/connectMongoDB.js";
-import { logger } from "./middleware/logger.js";
+import logger from "./middleware/logger.js";                  // default export
 import { notFoundHandler } from "./middleware/errorHandler.js"; // named export
 import errorHandler from "./middleware/errorHandler.js";        // default export
 
